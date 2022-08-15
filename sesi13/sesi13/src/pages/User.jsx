3029f5c615f656f30f758f526c8a83c8b3dd85ec
geoxemangoke <geoxemangoke@gmail.com>
2022-08-15 18:52:46 +0700

    sesi13

---
+++ sesi13/sesi13/src/pages/User.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+const User = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
+    return(
+        <div className="App">
+            <div>
+                <br />
+                <button>
+                    Back To Home
+                </button>
+            </div>
+            <br />
+            
+            <div>User List</div>
+            {users.map(user => {
+                return (
+                    <>
+                        <div>Name: {user.name}</div>   
+                        <div>Email: {user.email}</div>
+                        <br />
+                    </>
+                )
+            })}
+        </div>
+    )
+}
+
+export default User
