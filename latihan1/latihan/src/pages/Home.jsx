609aec69bc7ae48c84b2fb2c0093d01348fc26c9
geoxemangoke <geoxemangoke@gmail.com>
2022-08-29 07:50:03 +0700

    Fix link default

--- latihan1/latihan/src/pages/Home.jsx
+++ latihan1/latihan/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
 
 const Home = () => {
@@ -8,7 +9,9 @@ const Home = () => {
 
   useEffect(() => {
     async function getData() {
-      const res = await axios.get(`http://www.omdbapi.com/?apikey=a28eba38&s=avengers`);
+      const res = await axios.get(
+        `http://www.omdbapi.com/?apikey=a28eba38&s=avengers`
+      );
       setMovies(res.data.Search);
     }
     getData();
@@ -17,27 +20,37 @@ const Home = () => {
   return (
     <>
       <div className="container mt-3">
-        {
-        isLoading ? <Loading /> :
-          movies &&
-          <div className="fs-3 fw-bold">
-            <p>Show your favorites Movies</p>
-          </div>
-        }
+        {isLoading ? (
+          <Loading />
+        ) : (
+          movies && (
+            <div className="fs-3 fw-bold">
+              <p>Show your favorites Movies</p>
+            </div>
+          )
+        )}
         <div className="row row-cols-4">
-          {
-          movies.map((data, index) => (
-              <>
-                <div key={index} className="col-lg-3 align-items-stretch pb-4">
-                  <div className="card">
-                      <img src={data.Poster} className="card-img-top" alt={data.Title}/>
-                      <div className="card-body bg-dark text-center">
-                          <a href={`/about?id=${data.imdbID}`} className="card-link text-light">{data.Title}</a>
-                      </div>
+          {movies.map((data, index) => (
+            <>
+              <div key={index} className="col-lg-3 align-items-stretch pb-4">
+                <div className="card">
+                  <img
+                    src={data.Poster}
+                    className="card-img-top"
+                    alt={data.Title}
+                  />
+                  <div className="card-body bg-dark text-center">
+                    <Link
+                      to={`/about?id=${data.imdbID}`}
+                      className="card-link text-light"
+                    >
+                      {data.Title}
+                    </Link>
                   </div>
                 </div>
-              </>
-            ))}
+              </div>
+            </>
+          ))}
         </div>
       </div>
     </>
--- latihan1/latihan/src/pages/Search.jsx
+++ latihan1/latihan/src/pages/Search.jsx
@@ -8,32 +8,42 @@ const Search = (props) => {
   return (
     <>
       <div className="container mt-3">
-        {
-        isLoading ? 
+        {isLoading ? (
           <Loading />
-        :
-        props.list.Search && (
-          <div className="fs-3 fw-bold">
-            <p>Show your favorites Movies</p>
-          </div>
+        ) : (
+          props.list.Search && (
+            <div className="fs-3 fw-bold">
+              <p>Show your favorites Movies</p>
+            </div>
+          )
         )}
         <div className="row row-cols-4">
-          {props.list.Error ? 
-            <NoMatchError error={props.list.Error}/> 
-          :
-          props.list.Search &&
+          {props.list.Error ? (
+            <NoMatchError error={props.list.Error} />
+          ) : (
+            props.list.Search &&
             props.list.Search.map((imdb, index) => (
               <>
                 <div key={index} className="col-lg-3 align-items-stretch pb-4">
                   <div className="card">
-                      <img src={imdb.Poster} className="card-img-top" alt={imdb.Title}/>
-                      <div className="card-body bg-dark text-center">
-                          <Link to={`/about?id=${imdb.imdbID}`} className="card-link text-light">{imdb.Title}</Link>
-                      </div>
+                    <img
+                      src={imdb.Poster}
+                      className="card-img-top"
+                      alt={imdb.Title}
+                    />
+                    <div className="card-body bg-dark text-center">
+                      <Link
+                        to={`/about?id=${imdb.imdbID}`}
+                        className="card-link text-light"
+                      >
+                        {imdb.Title}
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </>
-            ))}
+            ))
+          )}
         </div>
       </div>
     </>
